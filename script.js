ca60f69f95b16ca18107e47e8cb6f2193cd844a8
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-05-17 17:32:25 +0200

    Added first basic drawing function

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 "use strict";
 
-// GENERAL VARIABLES
-let resolution = 16; // Resolution of the canvas and the default value
+// ---------- GENERAL VARIABLES ----------
+let resolution; // Resolution of the canvas and the default value
 
 // SELECTOR VARIABLES FOR HTML ELEMENTS
 const container = document.querySelector("#canvas-container"); // The container for the canvas
 const resolutionSlider = document.querySelector("#resolution-slider"); // The slider which sets the resolution of the grid
 const resolutionDisplay = document.querySelector("#resolution-display"); // The element that displays in realtime the current resolution set by the slider
+let canvasCells; // All the cells of the canvas
 
-// FUNCTIONS
+// ---------- FUNCTIONS ----------
 
 // This function creates the drawing canvas by generating a specified number of divs and appending them into the canvas container.
 function createCanvas(resolution) {
@@ -25,7 +26,28 @@ function createCanvas(resolution) {
   }
 }
 
-// Listens to the resolution slider. If it is moved, it updates the 'resolution' variable to the new value and also updates the resolution display accordingly.
+// Initialization
+function init() {
+  resolution = 16; // set default canvas resolution to 16x16
+  resolutionDisplay.textContent = `${resolution} x ${resolution}`; // Initializes the RESOLUTION DISPLAY to the default resolution value
+  createCanvas(resolution); // create canvas with the default resolution of 16x16
+  canvasCells = document.querySelectorAll(".canvas-cell"); // Get all canvas cells and save them in this variable
+  drawing();
+}
+
+// Change the color of each cell on mouse over.
+function drawing() {
+  canvasCells.forEach(
+    (cell) =>
+      (cell.onmouseover = function changeColor() {
+        cell.style.backgroundColor = "blue";
+      })
+  );
+}
+
+// ---------- RESOLUTION SLIDER FUNCTIONALITY ----------
+
+// Listens to the resolution slider. If it is moved, it updates the 'resolution' variable to the new value and also updates the onscreen resolution display accordingly.
 // This listener is set to "oninput", meaning the display will update IN REALTIME.
 resolutionSlider.oninput = function () {
   resolution = this.value;
@@ -36,12 +58,17 @@ resolutionSlider.oninput = function () {
 // To avoid bad performance and lag, which will occur when the resolution is updated with each new value, this is achieved with "onchange", which will only update once the mouse is released.
 resolutionSlider.onchange = function () {
   createCanvas(resolution);
+  canvasCells = document.querySelectorAll(".canvas-cell"); // Since there are now more/fewer cells, this variable has to be updated as well, otherwise the drawing on mouseover will not work for the newly added cells
+  drawing(); // Re-call the drawing function, so that the new canvasCells get applied to it.
 };
 
-// INITIALIZATIONS
-function init() {
-  resolutionDisplay.textContent = `${resolution} x ${resolution}`; // Initializes the RESOLUTION DISPLAY to the default resolution value
-  createCanvas(resolution);
-}
-
 init();
+
+// -------------TESTING AREA----------------
+
+// Test Button
+document.getElementById("reset").addEventListener("click", function () {
+  canvasCells = 0;
+  canvasCells = document.querySelectorAll(".canvas-cell");
+  console.log("pressed");
+});
